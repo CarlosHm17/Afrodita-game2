06f3c76df4c9a597d68a0be45e9b44d360422b9f
CarlosHm17 <candreshm03@gmail.com>
2023-01-10 12:49:17 -0500

    primer commit

---
+++ function.js
@@ -0,0 +1,27 @@
+const bootnU = document.getElementById("botonU")
+const bootnD = document.getElementById("botonD")
+const bootnT = document.getElementById("botonT")
+
+botonU.addEventListener("click", resultadoUno);
+
+function resultadoUno() {
+    botonU.classList.add("botonP");
+    botonD.classList.add("botonS");
+    botonT.classList.add("botonT");
+}
+
+botonD.addEventListener("click", resultadoDos);
+
+function resultadoDos() {
+    botonU.classList.add("botonS");
+    botonD.classList.add("botonP");
+    botonT.classList.add("botonT");
+}
+
+botonT.addEventListener("click", resultadoTres);
+
+function resultadoTres() {
+    botonU.classList.add("botonP");
+    botonD.classList.add("botonS");
+    botonT.classList.add("botonT");
+}
